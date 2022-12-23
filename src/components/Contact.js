a6870af54db7c73d80b78b8ca43c248f3e89ded8
Justice Dreischor <t-justice98@hotmail.com>
2022-12-23 13:17:40 +0100

    feat: update netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,7 +64,7 @@ const Contact = () => {
         </div>
         <form
           netlify
-          name="contact"
+          name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
