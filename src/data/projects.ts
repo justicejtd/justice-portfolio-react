ef53f646b739f21098acf1fb89404229f174df24
Justice Dreischor <t-justice98@hotmail.com>
2022-12-23 15:03:47 +0100

    feat: Updated testimonies

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,8 @@ export const projects = [
   {
     title: "Slam FM (Made by LiveWall)",
     subtitle: "Android App",
-    description: "This radio app was created by LiveWall for Slam FM radio station but during my traineeship at LiveWall my job was to track down and fix any bugs, crashes, and stability issues. One of my biggest challenge I faced so far came from this project, I had to come up with a solution to restart the streaming service from the notification bar after the app was killed.",
+    description:
+      "This radio app was created by LiveWall for Slam FM radio station but during my traineeship at LiveWall my job was to track down and fix any bugs, crashes, and stability issues. One of my biggest challenge I faced so far came from this project, I had to come up with a solution to restart the streaming service from the notification bar after the app was killed.",
     image: "./assets/gifs/slam-fm.gif",
     link:
       "https://play.google.com/store/apps/details?id=com.nakko.android.slamfm&hl=nl",
@@ -10,7 +11,8 @@ export const projects = [
   {
     title: "Global Talk Care (Made by LiveWall)",
     subtitle: "React Native App",
-    description: "The Global Talk Care is a type of a translation app that helps Dutch healthcare professionals communicate better with non-Dutch speaking patients. The patients can find their questions and answer them in their own language while it can be translated to Dutch. My job in this project was to add new features based on the client requirements.",
+    description:
+      "The Global Talk Care is a type of a translation app that helps Dutch healthcare professionals communicate better with non-Dutch speaking patients. The patients can find their questions and answer them in their own language while it can be translated to Dutch. My job in this project was to add new features based on the client requirements.",
     image: "./assets/gifs/global-talk-care.gif",
     link:
       "https://play.google.com/store/apps/details?id=nl.globaltalk.care&hl=nl",
@@ -18,7 +20,8 @@ export const projects = [
   {
     title: "DTT Real Estate",
     subtitle: "Android App",
-    description: "This Real Estate app can be used to find houses that are for sale and track their location using GPS and Google Maps. The app was specifically made for a traineeship assessment at DTT. It was implemented using clean architecture patterns and technologies such as Retrofit for API calls, Dagger Hilt for Dependency Injection and Room for local database. ",
+    description:
+      "This Real Estate app can be used to find houses that are for sale and track their location using GPS and Google Maps. The app was specifically made for a traineeship assessment at DTT. It was implemented using clean architecture patterns and technologies such as Retrofit for API calls, Dagger Hilt for Dependency Injection and Room for local database. ",
     image: "./assets/gifs/dtt-real-estate.gif",
     link: "https://github.com/justicejtd/dtt_houses",
   },
@@ -33,28 +36,32 @@ export const projects = [
   {
     title: "Spotify Clone",
     subtitle: "React Native App",
-    description: "During my traineeship at LiveWall I have built a Spotify clone demo app in order to demonstrate my learning results of the fundamentals of React Native, Redux, Redux Toolkit, RTK Query and custom components using Atomic Design. This app manages your favorite songs and controls your current playing song.",
+    description:
+      "During my traineeship at LiveWall I have built a Spotify clone demo app in order to demonstrate my learning results of the fundamentals of React Native, Redux, Redux Toolkit, RTK Query and custom components using Atomic Design. This app manages your favorite songs and controls your current playing song.",
     image: "./assets/gifs/spotify-clone.gif",
     link: "https://github.com/justicejtd/react-native-spotify-app",
   },
   {
     title: "PartiRecept",
     subtitle: "Flutter App",
-    description: "This app was created during the final phase project of my study along with other university colleagues. The concept behind this app is to connect people from different backgrounds that live or work at Kruistraat Eindhoven by sharing and finding nearby food recipes.",
+    description:
+      "This app was created during the final phase project of my study along with other university colleagues. The concept behind this app is to connect people from different backgrounds that live or work at Kruistraat Eindhoven by sharing and finding nearby food recipes.",
     image: "./assets/gifs/parti-recept.gif",
     link: "https://github.com/justicejtd/parti-recept",
   },
   {
     title: "Storycise",
     subtitle: "iOS App",
-    description: "Storycise is an exercise game app based on multiple immersive stories using audio and sensors from the Apple Watch. This app was created during my specialization along with my university colleagues.",
+    description:
+      "Storycise is an exercise game app based on multiple immersive stories using audio and sensors from the Apple Watch. This app was created during my specialization along with my university colleagues.",
     image: "./assets/gifs/storycise.gif",
     link: "https://github.com/justicejtd/Storycise_iOS",
   },
   {
     title: "Conveyor Belt Simulator",
     subtitle: "C# App",
-    description: "This conveyor belt simulator uses the Dijkstra Algorithm to simulate and find the shortest path a passenger baggage can take. This app was created using C# and XAML during my study core phase project along with other university colleagues.",
+    description:
+      "This conveyor belt simulator uses the Dijkstra Algorithm to simulate and find the shortest path a passenger baggage can take. This app was created using C# and XAML during my study core phase project along with other university colleagues.",
     image: "./assets/gifs/conveyor-belt-simulator.gif",
     link: "https://github.com/justicejtd/Conveyor_Belt_Simulator",
   },
@@ -72,7 +79,7 @@ export const testimonials = [
   // },
   {
     quote:
-      "Justice has been one of our most motivated interns during his study. It was therefore that a traineeship was offered in which he was able to learn new techniques and work on big brands like the SLAM! FM app. Justice showed to be a quick learner and portrayed a great deal of flexibility during this process.",
+      '"Justice has been one of our most motivated interns during his study. It was therefore that a traineeship was offered in which he was able to learn new techniques and work on big brands like the SLAM! FM app. Justice showed to be a quick learner and portrayed a great deal of flexibility during this process."',
     image: "./assets/images/man-avatar-placeholder.png",
     name: "Mathijs Bontje",
     role: "Team Lead",
@@ -82,7 +89,7 @@ export const testimonials = [
   },
   {
     quote:
-      "I (project manager) worked with Justice on an expansion of an already existing React Native project that he was unfamiliar with when he started. However, both during and in the week following the kickoff for the project, he asked all the right questions. He asked about the backstory, the goal of both the current project as well as the value of the expansion and the preferred way for me to communicate during the project so both our time would be used optimally. His skill and knowledge proved on par, where he basically delivered exactly what was asked. Additionally, he challenged me on certain choices that were  made before development and offered alternative solutions that actually made more sense and basically could be done in the same amount of time. Needless to say, the project was a success. How I experienced Justice on the workfloor is a person who keeps to himself most of the time, but uses that time to both work hard on development and improve his skill set.",
+      '"I (project manager) worked with Justice on an expansion of an already existing React Native project that he was unfamiliar with when he started. However, both during and in the week following the kickoff for the project, he asked all the right questions. He asked about the backstory, the goal of both the current project as well as the value of the expansion and the preferred way for me to communicate during the project so both our time would be used optimally. His skill and knowledge proved on par, where he basically delivered exactly what was asked. Additionally, he challenged me on certain choices that were  made before development and offered alternative solutions that actually made more sense and basically could be done in the same amount of time. Needless to say, the project was a success. How I experienced Justice on the workfloor is a person who keeps to himself most of the time, but uses that time to both work hard on development and improve his skill set."',
     image: "./assets/images/man-avatar-placeholder.png",
     name: "Matthijs Blanck",
     role: "Project Manager",
@@ -91,7 +98,8 @@ export const testimonials = [
     phone: "",
   },
   {
-    quote:"Justice is a very independent colleague who works precisely. He thinks critically and works with the team to come to a good solution for the problem at hand.",
+    quote:
+      '"Justice is a very independent colleague who works precisely. He thinks critically and works with the team to come to a good solution for the problem at hand."',
     image: "./assets/images/man-avatar-placeholder.png",
     name: "Remy Baratte",
     role: "Mobile developer",
